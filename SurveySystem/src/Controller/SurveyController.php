60a15a950b540b436264faecfe72069406b4f103
Dmksm <gg323824@gmail.com>
2022-04-23 10:42:33 +0300

    Added SurveyService, edited SurveyController, routes

--- SurveySystem/src/Controller/SurveyController.php
+++ SurveySystem/src/Controller/SurveyController.php
@@ -4,37 +4,29 @@ namespace App\Controller;
 
 use App\Module\Survey\RequestSurveyLoader;
 use App\Module\Survey\SurveyFileStorage;
+use App\Module\Survey\Service;
 use App\View\SurveyView;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 
 class SurveyController extends AbstractController
 {
+    private Service $service;
+
+    function __construct()
+    {
+        $this->service = new Service();
+    }
+
     public function saveSurvey(): Response
     {
-        $request = new RequestSurveyLoader();
-        $storage = new SurveyFileStorage();
-        $survey = $request->createNewSurvey('first_name', 'last_name', 'email', 'age');
-        $title = "Don't found email!";
-        if ($survey)
-        {
-            $storage->saveData($survey);
-            $title = "Done! Saved the survey:";
-        }
-        return $this->render(SurveyView::getTemplate(), SurveyView::getArgs($survey, $title));
+        $response = $this->service->saveSurvey();
+        return $this->render(SurveyView::getTemplate(), SurveyView::getArgs($response[0], $response[1]));
     }
 
     public function printSurvey(): Response
     {
-        $request = new RequestSurveyLoader();
-        $storage = new SurveyFileStorage();
-        $survey = $request->createNewSurvey('first_name', 'last_name', 'email', 'age');
-        $title = "Don't found email!";
-        if ($survey)
-        {
-            $storage->loadFileData($survey);
-            $title = "Loading old data from file:";
-        }
-        return $this->render(SurveyView::getTemplate(), SurveyView::getArgs($survey, $title));
+        $response = $this->service->printSurvey();
+        return $this->render(SurveyView::getTemplate(), SurveyView::getArgs($response[0], $response[1]));
     }
 }
---
+++ SurveySystem/src/Module/Survey/SurveyService.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Module\Survey;
+
+use App\Module\Survey\RequestSurveyLoader;
+use App\Module\Survey\SurveyFileStorage;
+
+class Service
+{
+    private RequestSurveyLoader $request;
+    private SurveyFileStorage $storage;
+
+    function __construct()
+    {
+        $this->request = new RequestSurveyLoader();
+        $this->storage = new SurveyFileStorage();
+    }
+
+    public function saveSurvey(): array
+    {
+        $survey = $this->request->createNewSurvey('first_name', 'last_name', 'email', 'age');
+        $title = "Don't found email!";
+        if ($survey)
+        {
+            $this->storage->saveData($survey);
+            $title = "Done! Saved the survey:";
+        }
+        return [$survey, $title];
+    }
+
+    public function printSurvey(): array
+    {
+        $survey = $this->request->createNewSurvey('first_name', 'last_name', 'email', 'age');
+        $title = "Don't found email!";
+        if ($survey)
+        {
+            $this->storage->loadFileData($survey);
+            $title = "Loading old data from file:";
+        }
+        return [$survey, $title];
+    }
+}
